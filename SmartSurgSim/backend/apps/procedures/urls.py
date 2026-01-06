089d56bd0c4055b7fa468057d6e2c8eb5112208b
Fabio Hams Valois <92942417+Neon18H@users.noreply.github.com>
2026-01-06 16:31:13 -0500

    Delete SmartSurgSim directory

--- SmartSurgSim/backend/apps/procedures/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ProcedureListView, ProcedureDetailView
-
-urlpatterns = [
-    path('procedures', ProcedureListView.as_view(), name='procedure_list'),
-    path('procedures/<int:pk>', ProcedureDetailView.as_view(), name='procedure_detail'),
-]
